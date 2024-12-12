afc3f41cdd319f6917a56579c85b5fe71742a64e
Julio Patiño <franjp132@gmail.com>
2024-12-12 15:00:11 -0500

    add bearer in axios instance

--- app/services/axios.ts
+++ app/services/axios.ts
@@ -4,6 +4,7 @@ const AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
   },
   timeout: 10000,
 });
