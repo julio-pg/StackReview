d224cb4e8a759cc930fbdcb5ac39285131336d4c
krabhi1 <abhishek33gl@gmail.com>
2024-12-24 04:18:18 +0530

    added filter by category and rating

---
+++ app/components/SelectBox.tsx
@@ -0,0 +1,38 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+
+export type SelectBoxParams = {
+  items: string[];
+  name: string;
+  value?: string;
+  format?: (value: string) => string;
+  onChange?: (value: string) => void;
+};
+export default function SelectBox({
+  items,
+  name,
+  value,
+  format,
+  onChange,
+}: SelectBoxParams) {
+  console.log("value", name, value);
+  return (
+    <Select onValueChange={onChange} value={value || ""}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={name} />
+      </SelectTrigger>
+      <SelectContent>
+        {items.map((value) => (
+          <SelectItem key={value} value={value}>
+            {format ? format(value) : value}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
